6a86d7e863c9e7b02e7d014156a3cc71429cc38e
Suvam Debnath <debnathsuvam322@gmail.com>
2026-02-28 09:36:57 +0530

    added all layers in ems-backend

---
+++ employee_management-backend/src/main/java/com/suvam/ems/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.suvam.ems.service;
+
+import com.suvam.ems.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
