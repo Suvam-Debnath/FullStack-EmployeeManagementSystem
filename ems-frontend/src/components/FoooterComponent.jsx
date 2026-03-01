56687878cd341459283a07727b961cda6bcd1e13
Suvam Debnath <debnathsuvam322@gmail.com>
2026-03-01 19:49:32 +0530

    configured routing in react

---
+++ ems-frontend/src/components/FoooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FoooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>
+                Developed by SuvamDebnath [springboot+reactjs]
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FoooterComponent
