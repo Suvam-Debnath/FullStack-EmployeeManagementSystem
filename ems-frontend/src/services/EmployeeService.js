f7d5aec0398c82ef73cf6636358cd23b7b0bfea0
Suvam Debnath <debnathsuvam322@gmail.com>
2026-03-01 21:11:50 +0530

    connected react app to add employee REST API

--- ems-frontend/src/services/EmployeeService.js
+++ ems-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const EMPLOYEE_API_BASE_URL = "http://localhost:8081/api/employees";
 
-export const listEmployees = () => {
-    return axios.get(EMPLOYEE_API_BASE_URL);
-}
+export const listEmployees = () => axios.get(EMPLOYEE_API_BASE_URL);
+export const createEmployee = (employee) => axios.post(EMPLOYEE_API_BASE_URL, employee);
+
