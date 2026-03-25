440782834189caa0983c60a42926d703e84df4f5
Suvam Debnath <debnathsuvam322@gmail.com>
2026-03-25 23:33:02 +0530

    added total count api [employee and department]

---
+++ employee_management-backend/src/main/java/com/suvam/ems/controller/DashboardController.java
@@ -0,0 +1,26 @@
+package com.suvam.ems.controller;
+
+import com.suvam.ems.dto.DashboardDto;
+import com.suvam.ems.repository.DepartmentRepository;
+import com.suvam.ems.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@AllArgsConstructor
+public class DashboardController {
+    private EmployeeRepository employeeRepository;
+    private DepartmentRepository departmentRepository;
+
+    @GetMapping
+    public ResponseEntity<DashboardDto> getDashboardData() {
+        DashboardDto dto = new DashboardDto();
+        dto.setEmployeeCount(employeeRepository.count());
+        dto.setDepartmentCount(departmentRepository.count());
+        return ResponseEntity.ok(dto);
+    }
+}
---
+++ employee_management-backend/src/main/java/com/suvam/ems/dto/DashboardDto.java
@@ -0,0 +1,9 @@
+package com.suvam.ems.dto;
+
+import lombok.Data;
+
+@Data
+public class DashboardDto {
+    private long employeeCount;
+    private long departmentCount;
+}
